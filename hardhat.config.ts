2e750b1e5a37dbc418ef85a5c64d1ed39dcd70e9
william89turner <william.turner.89@mail.ru>
2022-10-31 16:57:12 +0800

    refactor(init abis rule): export ABI to ./abis dir with hardhat-abi-exporter plugin

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,42 @@
+/*
+ * @Author: william89turner william.turner.89@mail.ru
+ * @Date: 2022-10-31 16:07:23
+ * @LastEditors: william89turner william.turner.89@mail.ru
+ * @LastEditTime: 2022-10-31 16:55:16
+ * @FilePath: /solib/hardhat.config.ts
+ * @Description: Supper Man Day Day Up!
+ */
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import 'hardhat-abi-exporter';
+import 'hardhat-spdx-license-identifier';
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.17",
+  solidity: {
+    version: '0.8.17',
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
+  abiExporter: {
+    path: './abis',
+    runOnCompile: true,
+    clear: true,
+    flat: true,
+    except: ['.*Mock$'],
+    spacing: 2,
+    format: "json",
+  },
+  gasReporter: {
+    enabled: true,
+  },
+  spdxLicenseIdentifier: {
+    overwrite: false,
+    runOnCompile: true,
+  },
 };
 
 export default config;
